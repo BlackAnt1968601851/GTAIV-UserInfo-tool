71822b0658366c841cfd65b95417551b7e8794ec
BlackAnt1968601851 <117094394+BlackAnt1968601851@users.noreply.github.com>
2024-02-02 05:46:03 -0500

    Initial Upload.

---
+++ UserInfo Decryptor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace YourNamespace
+{
+    // Your Security class
+    public class Security
+    {
+        public static byte[] Encrypt(byte[] unencryptedBytes)
+        {
+            return ProtectedData.Protect(unencryptedBytes, null, DataProtectionScope.CurrentUser);
+        }
+
+        public static byte[] Decrypt(byte[] encryptedBytes)
+        {
+            return ProtectedData.Unprotect(encryptedBytes, null, DataProtectionScope.CurrentUser);
+        }
+
+        // Method to load encrypted data from file
+        public static byte[] LoadEncryptedData(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllBytes(filePath);
+            }
+
+            return null;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load encrypted data from file
+            byte[] loadedEncryptedData = Security.LoadEncryptedData("userinfo.dat");
+
+            if (loadedEncryptedData != null)
+            {
+                // Decrypt loaded data
+                byte[] decryptedData = Security.Decrypt(loadedEncryptedData);
+
+                if (decryptedData != null)
+                {
+                    // Display the decrypted data
+                    string decryptedString = System.Text.Encoding.UTF8.GetString(decryptedData);
+                    Console.WriteLine("Decrypted Data: " + decryptedString);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserInfo Encryptor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UserInfoEncryptionTool
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // Specify the path to the XML file
+                string filePath = "UserInfo.txt";
+
+                // Read XML data from file
+                string xmlData = File.ReadAllText(filePath);
+
+                // Encrypt and save to file
+                byte[] encryptedBytes = Encrypt(Encoding.UTF8.GetBytes(xmlData));
+                File.WriteAllBytes("userinfo.dat", encryptedBytes);
+
+                Console.WriteLine("Encrypted data has been written to userinfo.dat");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        static byte[] Encrypt(byte[] unencryptedBytes)
+        {
+            return ProtectedData.Protect(unencryptedBytes, null, DataProtectionScope.CurrentUser);
+        }
+    }
+}
